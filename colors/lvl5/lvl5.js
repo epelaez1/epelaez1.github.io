dd12ea48fbc8ba7624a862755a78347014d3fc15
epelaez1 <enriquepelaez97@gmail.com>
2017-07-07 03:48:19 +0200

    boxes to colors

---
+++ colors/lvl5/lvl5.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	window.limitOfContainer = 3;
+	window.numberOfBoxes = 8;
+	window.numberOfColors = 6;
+	window.showSolutionSpeed = 350;
+	window.nextLvl = "lvl6"
+	window.doubleBox = false;
+	window.fakeBox = true;
+	start(numberOfColors,numberOfBoxes);
+	
+
+});
