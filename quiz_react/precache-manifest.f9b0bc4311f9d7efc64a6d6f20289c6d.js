1924c642b343ceaf1aaaa963b1c605834e8ba3b7
Enrique Peláez Cornejo <e.pelaez@alumnos.upm.es>
2019-10-17 19:27:34 +0200

    quiz_react added

---
+++ quiz_react/precache-manifest.f9b0bc4311f9d7efc64a6d6f20289c6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f21e7474f9844745c65680e654b338",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1eb668891801f705be25",
+    "url": "./static/css/main.0b216e12.chunk.css"
+  },
+  {
+    "revision": "0073aa4fcf70aead156f",
+    "url": "./static/js/2.9b2c5ec8.chunk.js"
+  },
+  {
+    "revision": "1eb668891801f705be25",
+    "url": "./static/js/main.40bedbb0.chunk.js"
+  },
+  {
+    "revision": "932b8a9fd43d140396ac",
+    "url": "./static/js/runtime-main.a12b0401.js"
+  }
+]);
